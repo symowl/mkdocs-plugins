863225a1db06c736ca6f6cb2ee55d662f2ad9e55
Symowl <13601121148@163.com>
2022-12-19 22:27:14 +0800

    1.9.0

--- smp/__init__.py
+++ smp/__init__.py
@@ -1,6 +1,5 @@
-"""Symowl MkDocs Plugins
-Copyright (c) 2022 Symowl
-SPDX-License-Identifier: Apache-2.0
-"""
+# Symowl MkDocs Plugins
+# Copyright (c) 2022 Symowl
+# SPDX-License-Identifier: Apache-2.0
 
 __version__ = "1.9.0"
